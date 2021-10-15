ef7d5eac3d48c5078a61c166cae788ec3698d0ef
tomiivazqueznwn12 <72195572+tomiivazqueznwn12@users.noreply.github.com>
2021-10-15 14:10:13 -0300

    Update main.py
    function update and delete added

--- main.py
+++ main.py
@@ -16,6 +16,22 @@ def list_clients():
 
     print(clients)
 
+def update_client(client_name, updated_client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',',updated_client_name)
+    else:
+        print('that client isn´t in the list')
+
+def delete_client_name(client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',','')
+    else:
+        print('clients is not in client list')
+
 
 def _add_comma():
     global clients
@@ -29,19 +45,30 @@ def _print_welcome():
     print('what would you like to do today?')
     print('[C]reate client')
     print('[D]elete client')
+    print('[U]pdate client')
 
+def _get_client_name():
+    return(input('What is the client name? '))
 
 if __name__ == "__main__":
     _print_welcome()
 
     command = input()
+    command = command.upper()
 
     if command == 'C':
-        client_name = input('What is the client name? ')
+        client_name = _get_client_name()
         create_clients(client_name)
         list_clients()
     elif command == 'D':
-        pass
+        client_name = _get_client_name()
+        delete_client_name(client_name)
+        list_clients()
+    elif command == 'U':
+        client_name = _get_client_name()
+        updated_client_name = input('What is the updated client name ')
+        update_client(client_name, updated_client_name)
+        list_clients()
     else:
         print('Invalid command ')
 
